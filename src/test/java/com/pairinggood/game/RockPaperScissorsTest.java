7348e1c2c62cbc81bfee209421f7e805496e78a7
Tyler Ward <tylerjward@gmail.com>
2019-02-25 19:08:46 -0600

    rock beats scissor

--- src/test/java/com/pairinggood/game/RockPaperScissorsTest.java
+++ src/test/java/com/pairinggood/game/RockPaperScissorsTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 public class RockPaperScissorsTest {
 
     @Test
-    public void play_RockBeatsScissors() {
+    public void play_RockThenScissor_RockBeatsScissors() {
         //arrange
         RockPaperScissors game = new RockPaperScissors();
 
@@ -17,4 +17,17 @@ public class RockPaperScissorsTest {
         //assert
         assertEquals("rock", winner);
     }
+
+    @Test
+    public void play_ScissorThenRock_RockBeatsScissors() {
+        //arrange
+        RockPaperScissors game = new RockPaperScissors();
+
+        //act
+        String winner = game.play("scissors", "rock");
+
+        //assert
+        assertEquals("rock", winner);
+    }
+
 }
